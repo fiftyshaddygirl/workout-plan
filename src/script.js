7083664e162460a8a51ea5dd9865976285e61a9c
cynkzn <cynkazan@gmail.com>
2025-04-14 00:11:40 +1000

    remove console.log and update css style

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function displayWorkout(response) {
-  console.log(response.data);
   let aiWorkoutPlan = document.querySelector("#workout-plan");
   aiWorkoutPlan.innerHTML = response.data.answer;
 }
@@ -7,11 +6,12 @@ function displayWorkout(response) {
 function workoutPlan(event) {
   event.preventDefault();
   let instruction = document.querySelector("#user-instruction");
-  console.log(instruction.value);
+  let aiWorkoutPlan = document.querySelector("#workout-plan");
+  aiWorkoutPlan.innerHTML = `Generating ${instruction.value} workout plan. Please wait . . .`;
 
   let apiKey = "940836f57095o1a5bt3c042d91bb08f9";
   let context =
-    "You are a knowledgeable personal trainer that love to help other to get fit in basic HTML value and separate line by <br> and make sure to follow user instruction";
+    "You are a knowledgeable personal trainer that love to help other to build muscle in basic HTML value and separate line by <br> and make sure to follow user instruction";
   let prompt = `user instruction : display workout for ${instruction.value} in precise with no explanation. always add title at the begining`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayWorkout);
