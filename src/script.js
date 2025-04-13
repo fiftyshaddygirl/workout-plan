da10767d010bc505d9dd959328708b0b8d5d17d8
cynkzn <cynkazan@gmail.com>
2025-04-14 00:23:42 +1000

    update AI context

--- src/script.js
+++ src/script.js
@@ -16,8 +16,8 @@ function workoutPlan(event) {
   });
   let apiKey = "940836f57095o1a5bt3c042d91bb08f9";
   let context =
-    "You are a knowledgeable personal trainer that love to help other to build muscle in basic HTML value and separate line by <br> and make sure to follow user instruction";
-  let prompt = `user instruction : display workout for ${instruction.value} in precise with no explanation. always add title at the begining`;
+    "You are a knowledgeable personal trainer that love to help other to build muscle in basic HTML and separate line by <br> and make sure to follow user instruction";
+  let prompt = `user instruction : display workout for ${instruction.value} and repetition in precise with no explanation. always add title at the begining`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayWorkout);
   console.log("Calling API data");
