f7ae0daeb3b21f884e0aea37c96b2e63e995a27a
cynkzn <cynkazan@gmail.com>
2025-04-14 00:21:35 +1000

    adding js plugin : typewriter

--- src/script.js
+++ src/script.js
@@ -6,9 +6,14 @@ function displayWorkout(response) {
 function workoutPlan(event) {
   event.preventDefault();
   let instruction = document.querySelector("#user-instruction");
-  let aiWorkoutPlan = document.querySelector("#workout-plan");
-  aiWorkoutPlan.innerHTML = `Generating ${instruction.value} workout plan. Please wait . . .`;
+  aiWorkoutPlan = `Generating ${instruction.value} workout plan. Please wait . . .`;
 
+  new Typewriter("#workout-plan", {
+    strings: aiWorkoutPlan,
+    autoStart: true,
+    cursor: null,
+    delay: 25,
+  });
   let apiKey = "940836f57095o1a5bt3c042d91bb08f9";
   let context =
     "You are a knowledgeable personal trainer that love to help other to build muscle in basic HTML value and separate line by <br> and make sure to follow user instruction";
